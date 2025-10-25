4277a5a78d27f571776072011c3b22e1cef83315
Fares Aouani Cherif <fares.aouanicherif@mazars.de>
2025-10-25 23:58:59 +0200

    edit heimlab docs

--- src/app/docs/setup-cloudflare/page.tsx
+++ src/app/docs/setup-cloudflare/page.tsx
@@ -1,5 +1,19 @@
 const SetupCloudflare = () => {
-  return <>works</>;
+  return (
+    <div className="text-xl">
+      Please check the{" "}
+      <a
+        href="https://docs.ferez.cloud/tunneling"
+        className="text-3xl underline"
+      >
+        Tunneling Documentation
+      </a>{" "}
+      page to setup your tunnels.
+      <br />
+      Each of your docker service can be connected to a subdomain and accessible
+      on Internet, securely!
+    </div>
+  );
 };
 
 export default SetupCloudflare;
